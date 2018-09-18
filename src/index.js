2e427937a4496015c0c3f005345cf473f0db11a1
FernanityChamp <andres.fernando08@gmail.com>
2018-09-17 22:29:49 -0500

    import actions and we call written methods in the store config

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import store from './store/configureStore';
+import { Provider } from 'react-redux';
+import { getCharacters } from './actions/actionsTypes';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+store.dispatch(getCharacters())
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
