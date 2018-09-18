146f49cad33a9eb9615bd5624758c3034e2882fd
FernanityChamp <andres.fernando08@gmail.com>
2018-09-17 22:30:50 -0500

    we iterate data returned by the store and is showed in the App component throughout Character component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
+import { connect } from 'react-redux';
 import Character from './components/Character/Character';
 
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h3>Bienvenidos a funApi</h3>
-        <Character name="Andres Fernando" image="" />
-      </div>
-    );
+const App = ({characters}) => (
+  <div className="App">
+    <h3>Bienvenidos a funApi</h3>
+    {characters.map(char =>
+      <Character
+        key={char.id} 
+        name={char.name} image={char.image} />
+    )}
+  </div>
+)
+
+const mapStateToProps = (state) => {
+  return {
+    characters: state.characters
   }
 }
 
-export default App;
+export default connect(mapStateToProps)(App);
